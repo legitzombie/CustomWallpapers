fc876ceb087e93e9e02417e684ef34f5b067441a
JamesTheHacker <56886589+legitzombie@users.noreply.github.com>
2025-07-30 13:17:10 -0400

    Add files via upload

--- Utils/GETDOTNET.bat
+++ Utils/GETDOTNET.bat
@@ -1,16 +1,16 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
 set "runbat=%~dp0..\Run.bat"
 
 set "sdk_path=%ProgramFiles%\dotnet\sdk"
 if exist "%sdk_path%" (
-    goto :install
+    exit /b 0
 )
 
 echo [INFO] .NET SDK not found. Downloading installer...
 
-set "installer_name=%TEMP%\dotnet-sdk-8.0.412-win-x64.exe"
+for %%i in ("%TEMP%\dotnet-sdk-8.0.412-win-x64.exe") do set "installer_name=%%~fi"
 set "dotnet_installer_url=https://builds.dotnet.microsoft.com/dotnet/Sdk/8.0.412/dotnet-sdk-8.0.412-win-x64.exe"
 
 curl -L -o "%installer_name%" "%dotnet_installer_url%"
@@ -21,40 +21,16 @@ if not exist "%installer_name%" (
 )
 
 echo [INFO] Installing .NET SDK... (may prompt for UAC)...
-start /wait "" "%installer_name%" /install /quiet /norestart
+"%installer_name%" /install /quiet /norestart
 
-set "retries=30"
-set /a count=0
-
-:check_dotnet
-if exist "%sdk_path%" (
-    echo [SUCCESS] .NET SDK installed successfully!
-    del "%installer_name%"
-    goto :done
-)else (
-	cls
-	echo [INFO] Installing .NET SDK... (may prompt for UAC)...
-	set "line="
-
-	for /L %%i in (1,1,%count%) do (
-		set "line=!line!*"
-	)
-
-	echo Progress: !line!
-)
-
-timeout /t 2 >nul
-set /a count+=1
-if %count% LSS %retries% goto :check_dotnet
-
-echo [FAIL] .NET SDK install timed out or failed.
-exit /b 1
+echo [SUCCESS] .NET SDK installed successfully!
+del "%installer_name%"
+goto :done
 
 :done
 echo [INFO] .NET is ready.
 start "" cmd /k ""%runbat%""
 exit /b 123
 
-:install
 exit /b 0
 endlocal
--- Utils/GETPYTHON.bat
+++ Utils/GETPYTHON.bat
@@ -1,7 +1,9 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set "python_exe=%LOCALAPPDATA%\Programs\Python\Python312\python.exe"
+set "runbat=%~dp0..\Run.bat"
+
+set "python_exe=%ProgramFiles%\Python312\python.exe"
 if exist "%python_exe%" (
     exit /b 0
 )
@@ -11,35 +13,13 @@ echo [INFO] Python not found. Downloading installer...
 for %%i in ("%TEMP%\python_installer.exe") do set "temp_installer=%%~fi"
 curl -o "%temp_installer%" https://www.python.org/ftp/python/3.12.3/python-3.12.3-amd64.exe
 
-
 echo [INFO] Installing Python... (may prompt for UAC)...
 "%temp_installer%" /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
 
+timeout /t 5 >nul
 
-set "retries=30"
-set /a count=0
-
-:check_python
-if exist "%python_exe%" (
-    echo [SUCCESS] Python installed at "%python_exe%"
-    goto :done
-)else (
-	cls
-	echo [INFO] Installing Python... (may prompt for UAC)...
-	set "line="
-
-	for /L %%i in (1,1,%count%) do (
-		set "line=!line!*"
-	)
-
-	echo Progress: !line!
-)
-timeout /t 2 >nul
-set /a count+=1
-if !count! LSS !retries! goto :check_python
-
-echo [FAIL] Python install timed out or failed.
-exit /b 1
+echo [SUCCESS] Python installed at "%python_exe%"
+goto :done
 
 :done
 echo [INFO] Python is ready.
