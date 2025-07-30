bc38dc917a572b650b801f5175519d8bb629485c
JamesTheHacker <56886589+legitzombie@users.noreply.github.com>
2025-07-30 14:54:56 -0400

    Add files via upload

--- Utils/CREATEFOLDERS.bat
+++ Utils/CREATEFOLDERS.bat
@@ -6,19 +6,19 @@ set "script_path=%~dp0"
 for %%i in ("%script_path%\..") do set "temp_path=%%~fi\temp"
 
 if not exist %temp_path% (
-	mkdir %temp_path%
-	mkdir %temp_path%\images
-	mkdir %temp_path%\raw\base\customwallpapers
-	mkdir %temp_path%\archive\base\customwallpapers
+	mkdir "%temp_path%"
+	mkdir "%temp_path%\images"
+	mkdir "%temp_path%\raw\base\customwallpapers"
+	mkdir "%temp_path%\archive\base\customwallpapers"
 ) 
 
 for %%i in ("%script_path%\..") do set "perm_path=%%~fi\archive\pc\mod"
 for %%i in ("%script_path%\..") do set "root_path=%%~fi\archive"
 
-if exist %root_path% (
+if exist "%root_path%" (
 	rmdir /s /q "%root_path%"
 )
 
-mkdir %perm_path%
+mkdir "%perm_path%"
 
 endlocal
